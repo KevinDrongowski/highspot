52aad5fe12ba5a79fee260473d51f82a67d90c2b
Kevin Drongowski <xiaphas@gmail.com>
2020-04-15 15:02:45 -0700

    Updates

---
+++ precache-manifest.affc6c787c616549f602ead33f3c8503.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e5ea531d9ea576c38d0f41373fb7d4",
+    "url": "/highspot/index.html"
+  },
+  {
+    "revision": "cde8a026ec0fbef5fd34",
+    "url": "/highspot/static/css/main.0fcf16a1.chunk.css"
+  },
+  {
+    "revision": "872d5dfc5bc598ec06a1",
+    "url": "/highspot/static/js/2.9d7cf96d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/highspot/static/js/2.9d7cf96d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde8a026ec0fbef5fd34",
+    "url": "/highspot/static/js/main.625c59b7.chunk.js"
+  },
+  {
+    "revision": "adccd7282e6a18e33f6d",
+    "url": "/highspot/static/js/runtime-main.7bd49766.js"
+  },
+  {
+    "revision": "21f887519b60c0884162222f5f4668f4",
+    "url": "/highspot/static/media/elder-scrolls-bg.21f88751.jpg"
+  },
+  {
+    "revision": "b9d0a541e0be5e8a51b7c4df1be45c76",
+    "url": "/highspot/static/media/esl-logo-latest.b9d0a541.png"
+  },
+  {
+    "revision": "750fc7f7c6391b7ec37212f9ae002875",
+    "url": "/highspot/static/media/loading.750fc7f7.gif"
+  }
+]);
